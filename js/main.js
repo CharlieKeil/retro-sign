05f732241d96c1bf610a79f830f5b830fe584a62
CharlieKeil <Keil0009@algonquinlive.com>
2016-04-05 15:18:33 -0400

    Changed some code.

--- js/main.js
+++ js/main.js
@@ -1,11 +1,5 @@
 var $svg = $('svg');
-var $lines = $('.lines');
-var $engage = $('.engage');
-var $btn = $('.btn');
 
-$('svg').on('click', function (e) {
-  $('.lines').toggleClass('linify');
-  $('.engage').toggleClass('engaged');
-  $btn.toggleClass('button');
-  $('svg').toggleClass('is-pressed');
+$('btn').on('click', function () {
+  $svg.toggleClass('in-engaged');
 });
